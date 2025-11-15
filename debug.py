688d99dafc1a800deb572d9f135869267aa90f94
jbafr122 <jeremiahbafr122@gmail.com>
2025-11-15 13:12:48 -0600

    answered debug module questions

---
+++ debug.py
@@ -0,0 +1,36 @@
+#   1.  Write an assert statement that triggers an AssertionError if the variable spam is an integer less than 10.
+
+# spam = 1
+# assert spam >= 10
+
+#   2.  Write an assert statement that triggers an AssertionError if the variables eggs and bacon contain strings that are the same as each other, even if their cases are different. (That is, 'hello' and 'hello' are considered the same, as are 'goodbye' and 'GOODbye'.)
+
+#bacon = "hello"
+#eggs = "Hello"
+#assert bacon.lower() != eggs.lower()
+
+#   3.  Write an assert statement that always triggers an AssertionError.
+
+assert False
+
+#   4.  What two lines must your program have to be able to call logging.debug()?
+
+#   5.  What two lines must your program have to make logging.debug() send a logging message to a file named programLog.txt?
+
+#   6.  What are the five logging levels?
+
+#   7.  What line of code can you add to disable all logging messages in your program?
+
+#   8.  Why is using logging messages better than using print() to display the same message?
+
+#   9.  What are the differences between the Step Over, Step In, and Step Out buttons in the debugger?
+
+# 10.  After you click Continue, when will the debugger stop?
+
+# 11.  What is a breakpoint?
+
+# 12.  How do you set a breakpoint on a line of code in Mu?
+
+
+
+
