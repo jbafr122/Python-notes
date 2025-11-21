881f12b5676ca87a4945c7d313790a1ae70a1498
jbafr122 <jbafr122@gmail.com>
2025-11-21 22:54:23 +0000

    finished string formatting

--- python-stuff/push.sh
+++ python-stuff/push.sh
@@ -1,9 +1,11 @@
 # pushes changes to github repo and asks the user for a commit msg
 # use "sh push.sh" or whatever shell script name is to run script 
 
+cd ~/Python-notes/
 git add .
 echo "What would you like the commit message to be?" 
 read msg
 git commit -m "$msg"
 git push
 echo "Successfully pushed to git repo"
+cd ~/Python-notes/python-stuff
